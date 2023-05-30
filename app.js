48100b00c6f136a9dd0ee5c570cc2094a2bf82e5
Edith Werbel <edith.werbel@gmail.com>
2023-05-29 21:15:29 -0400

    added column and diagonal functions

--- app.js
+++ app.js
@@ -65,23 +65,47 @@ function processBoard(gameboard) {
         endGame('There is no winner');
     }
 }
-// should return true or false
 function isRowComplete(gameboard) {
     for (let i = 0; i < gameboard.length; i++) {
         const row = gameboard[i];
         if (row.every(square => square === row[0] && row[0] !== null)) {
-            console.log("There's a row winner")
-            // convert false to "O" and true to "X"
-            let winner = "";
-            if (row[0] === false) {
-                winner = "O";
-            } else {
-                winner = "X";
-            }
-            console.log(winner)
-            return winner; // Return the string of either "x" or "o"
+            return OorXforTheWin(row[0]);
         }
     }
+}
+
+function isColumnComplete(gameboard) {
+    for (let i = 0; i < gameboard.length; i++) {
+        const column = gameboard.map(row => row[i]);
+        if (column.every(square => square === column[0] && column[0] !== null)) {
+            return OorXforTheWin(column[0]);
+        }
+    }
+}
+
+function isDiagonalComplete(gameboard) {
+    const diagonal1 = [gameboard[0][0], gameboard[1][1], gameboard[2][2]];
+    if (diagonal1.every(square => square === diagonal1[0] && diagonal1[0] !== null)) {
+        console.log("there's a diagonal winner")
+        return OorXforTheWin(diagonal1[0]);
+    }
+
+    const diagonal2 = [gameboard[0][2], gameboard[1][1], gameboard[2][0]];
+    if (diagonal2.every(square => square === diagonal2[0] && diagonal2[0] !== null)) {
+        console.log("there's a diagonal winner")
+        return OorXforTheWin(diagonal2[0]);
+    }
+}
+
+// convert fasle to "O" and true to "x"
+function OorXforTheWin(bool) {
+    let winner = "";
+    if (bool === false) {
+        winner = "O";
+    } else {
+        winner = "X";
+    }
+    return winner;
 }
 
 function endGame(winner) {
